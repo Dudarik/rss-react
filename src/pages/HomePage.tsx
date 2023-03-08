13bc0cf0fe0a7e60b22c106ba01a1f57d6d08623
Dudarik <dudarik@yandex.ru>
2023-03-08 08:03:29 +0500

    refactor: MainPage -> HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <main className="main">
+        <h1>This is a HOME page</h1>
+      </main>
+    );
+  }
+}
+
+export default HomePage;
