bda02f04903b0acc4e8a392325d1234ca7f8f49e
Dudarik <dudarik@yandex.ru>
2023-03-10 08:17:48 +0500

    feat: add styles to main menu

--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -1,19 +1,36 @@
 import React, { Component, ReactNode } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import styles from './mainnav.module.scss';
+
 class MainNav extends Component {
   render(): ReactNode {
     return (
       <nav>
-        <ul>
+        <ul className={styles.menu}>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? styles.active : styles.menu_link)}
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/about">About us</NavLink>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? styles.active : styles.menu_link)}
+            >
+              About us
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/item">item</NavLink>
+            <NavLink
+              to="/item"
+              className={({ isActive }) => (isActive ? styles.active : styles.menu_link)}
+            >
+              for 404
+            </NavLink>
           </li>
         </ul>
       </nav>
