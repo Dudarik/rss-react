1feef656c40e24337bf54d931093985f052b9049
Dudarik <Dudarik@yandex.ru>
2023-03-10 12:40:41 +0500

    refactor: card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -15,25 +15,46 @@ class Card extends Component<IGameData> {
   render(): ReactNode {
     const { id, title, players, age, playingTime, lang, image } = this.props;
 
+    const infoData = [
+      {
+        icon: 'fa-solid fa-person',
+        title: 'Кол-во игроков:',
+        title_info: players,
+      },
+      {
+        icon: 'fa-solid fa-clock',
+        title: 'Время партии:',
+        title_info: playingTime,
+      },
+      {
+        icon: 'fa-solid fa-child-reaching',
+        title: 'Возраст:',
+        title_info: `от ${age} лет`,
+      },
+      {
+        icon: 'fa-solid fa-language',
+        title: 'Язык:',
+        title_info: lang,
+      },
+    ];
+
     return (
       <li className={styles.card}>
         <img src={PATH_TO_IMG + image} alt={title} width="200" />
         <Link to={`products/${id}`} className={styles.card_title}>
           {title}
         </Link>
+
         <div className={styles.info}>
-          <span>
-            <i className="fa-solid fa-person"></i> Кол-во игроков: {players}
-          </span>
-          <span>
-            <i className="fa-solid fa-clock"></i> Время партии: {playingTime} мин.
-          </span>
-          <span>
-            <i className="fa-solid fa-child-reaching"></i> Возраст: от {age} лет
-          </span>
-          <span>
-            <i className="fa-solid fa-language"></i> Язык: {lang}
-          </span>
+          {infoData.map((infoItem) => {
+            const { icon, title, title_info } = infoItem;
+            return (
+              <span className={styles.info_item} key={icon}>
+                <i className={icon}></i> {title}
+                <span className={styles.item_data}> {title_info}</span>
+              </span>
+            );
+          })}
         </div>
       </li>
     );
