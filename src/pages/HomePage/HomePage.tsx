e30b87b8a5c9f928deff6685b033e64203f338a2
Dudarik <dudarik@yandex.ru>
2023-03-10 09:00:02 +0500

    feat: add styles to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,11 +2,13 @@ import CardsList from '../../components/CardsList';
 import SearchBar from '../../components/SearchBar';
 import React, { Component } from 'react';
 
+import styles from './homepage.module.scss';
+
 class HomePage extends Component {
   render() {
     return (
-      <main className="main">
-        <h1>This is a HOME page</h1>
+      <main className={styles.main}>
+        <h2>This is a HOME page</h2>
         <SearchBar />
         <CardsList />
       </main>
