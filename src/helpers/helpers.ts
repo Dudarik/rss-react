d5356c5eaaddc0e2773645d6486c9d227991f2e4
Dudarik <Dudarik@yandex.ru>
2023-03-11 21:04:52 +0500

    feat: add test to helpers

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -4,13 +4,4 @@ import db from '../db/db.json';
 export const getGamesData = (): IGamesData | undefined => {
   if (db) return db;
   return undefined;
-  // try {
-  //   const response = await fetch();
-  //   if (response.status !== 200) throw new Error(`Can't load data from DB`);
-
-  //   return (await response.json()) as IGamesData;
-  // } catch (error) {
-  //   console.log(error);
-  //   return undefined;
-  // }
 };
