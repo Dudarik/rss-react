9fb037cc09cc50a64862d0976810b8d351701d8a
Dudarik <Dudarik@yandex.ru>
2023-03-11 20:35:04 +0500

    refactor: main menu

--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -3,27 +3,28 @@ import { NavLink } from 'react-router-dom';
 
 import styles from './MainNav.module.scss';
 
+interface IMenuLink {
+  isActive: boolean;
+}
+
 class MainNav extends Component {
   render(): ReactNode {
+    const isActiveLink = (link: IMenuLink) => (link.isActive ? styles.active : styles.menu_link);
+    const menu = [
+      { path: '/', title: 'Home' },
+      { path: '/about', title: 'About us' },
+    ];
+
     return (
       <nav>
         <ul className={styles.menu}>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? styles.active : styles.menu_link)}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/about"
-              className={({ isActive }) => (isActive ? styles.active : styles.menu_link)}
-            >
-              About us
-            </NavLink>
-          </li>
+          {menu.map((menuItem) => (
+            <li key={menuItem.title}>
+              <NavLink to={menuItem.path} className={isActiveLink}>
+                {menuItem.title}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
     );
