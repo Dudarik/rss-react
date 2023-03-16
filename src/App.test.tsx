193159544fd7c4a450bc5b85f12ddb30a202fc41
Dudarik <Dudarik@yandex.ru>
2023-03-16 12:47:02 +0500

    feat: add more configs

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,16 @@
 import { describe, test, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Footer from '.';
+import App from './App';
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 
-describe('<Footer />', () => {
-  test('Header mounts properly', () => {
-    const wrapper = render(<Footer />);
+describe('<App />', () => {
+  test('App mounts properly', () => {
+    const wrapper = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
     expect(wrapper).toBeTruthy();
 
     const text = screen.getByText(/Alex Reznichenko/i);
