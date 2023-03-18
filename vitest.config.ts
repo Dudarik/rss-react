840e087706e7f547cbc7b23eed0200268a9ba838
Dudarik <Dudarik@yandex.ru>
2023-03-18 14:55:35 +0500

    refactor: config file, minor changes

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,35 +2,28 @@
 
 import { defineConfig } from 'vitest/config';
 
+const exclude = [
+  '**/node_modules/**',
+  '**/dist/**',
+  '**/src/interfaces/**',
+  '**/cypress/**',
+  '**/.{idea,git,cache,output,temp}/**',
+  '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+  '**/*.d.ts',
+  '**/main.tsx',
+];
+
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
-    exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/src/interfaces/**',
-      '**/cypress/**',
-      '**/.{idea,git,cache,output,temp}/**',
-      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-      '**/*.d.ts',
-      '**/main.tsx',
-    ],
+    exclude,
     coverage: {
       provider: 'c8',
       all: true,
       reporter: 'text',
-      exclude: [
-        '**/node_modules/**',
-        '**/dist/**',
-        '**/src/interfaces/**',
-        '**/cypress/**',
-        '**/.{idea,git,cache,output,temp}/**',
-        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-        '**/*.d.ts',
-        '**/main.tsx',
-      ],
+      exclude,
     },
   },
 });
