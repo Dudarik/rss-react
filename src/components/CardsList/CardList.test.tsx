c89e503b77fc4c4f20f773a634edbf73958c9279
Dudarik <Dudarik@yandex.ru>
2023-03-21 13:28:54 +0500

    refactor: CardList test

--- src/components/CardsList/CardList.test.tsx
+++ src/components/CardsList/CardList.test.tsx
@@ -6,11 +6,39 @@ import { MemoryRouter } from 'react-router-dom';
 
 import CardsList from '.';
 
+const cardsData = {
+  publishers: [
+    {
+      id: 0,
+      title: 'Лавка игр',
+      img: 'lavkagames.webp',
+    },
+  ],
+  games: [
+    {
+      id: 0,
+      title: 'Архитекторы западного королевства',
+      releaseDate: '25.12.2015',
+      publisher: 0,
+      players: '1 - 5',
+      playingTime: '60 - 90',
+      age: 12,
+      lang: 'Русский',
+      scoreBGG: 7.77,
+      scoreTesera: 7.77,
+      image: 'architech.webp',
+      game: true,
+      publisherTitle: 'Лавка игр',
+      publisherImage: 'lavkagames.webp',
+    },
+  ],
+};
+
 describe('<CardList />', () => {
   test('Card mounts properly', () => {
     const wrapper = render(
       <MemoryRouter>
-        <CardsList />
+        <CardsList {...cardsData} />
       </MemoryRouter>
     );
 
