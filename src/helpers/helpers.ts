eacd7f28c424aaaa92dd4e3c5e7a6c4f9726f549
Dudarik <Dudarik@yandex.ru>
2023-03-21 13:16:57 +0500

    refactor: getGamesData, add params

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,7 +1,13 @@
-import { IGamesData } from '../interfaces/cardsIterfaces';
+import { IGameData, IGamesData, IPublisher } from '../interfaces/cardsIterfaces';
 import db from '../db/db.json';
 
-export const getGamesData = (): IGamesData | undefined => {
-  if (db) return db;
+type TReturnGamesData = IGamesData | IGameData[] | IPublisher[] | undefined;
+
+export const getGamesData = (key?: keyof IGamesData): TReturnGamesData => {
+  const data = db as IGamesData;
+  if (data) {
+    if (key) return data[key];
+    return data;
+  }
   return undefined;
 };
