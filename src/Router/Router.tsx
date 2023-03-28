f77c577ac2f1a5cb08858a91fb1d3158b7618bce
Dudarik <Dudarik@yandex.ru>
2023-03-28 16:34:57 +0500

    refactor: Router from class to hooks

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { HomePage, AboutPage, FormPage, NotFoundPage } from '../pages';
@@ -7,43 +7,38 @@ interface IRouterProps {
   setCurrentPageTitle?: (newTitle: string) => void;
 }
 
-class Router extends Component<IRouterProps> {
-  render() {
-    const { setCurrentPageTitle } = this.props;
-    const routes = [
-      {
-        path: '/',
-        element: <HomePage pageTitle="Home" setCurrentPageTitle={setCurrentPageTitle} />,
-      },
-      {
-        path: '/about',
-        element: <AboutPage pageTitle="About" setCurrentPageTitle={setCurrentPageTitle} />,
-      },
-      {
-        path: '/form',
-        element: <FormPage pageTitle="Form" setCurrentPageTitle={setCurrentPageTitle} />,
-      },
-      {
-        path: '/404',
-        element: (
-          <NotFoundPage pageTitle="Not Found(404)" setCurrentPageTitle={setCurrentPageTitle} />
-        ),
-      },
-      {
-        path: '*',
-        element: <Navigate to="/404" replace />,
-      },
-    ];
-
-    return (
-      <Routes>
-        {routes.map((route) => {
-          const { path, element } = route;
-          return <Route path={path} element={element} key={path} />;
-        })}
-      </Routes>
-    );
-  }
-}
+const Router = (props: IRouterProps) => {
+  const { setCurrentPageTitle } = props;
+  const routes = [
+    {
+      path: '/',
+      element: <HomePage {...{ pageTitle: 'Home', setCurrentPageTitle }} />,
+    },
+    {
+      path: '/about',
+      element: <AboutPage {...{ pageTitle: 'About', setCurrentPageTitle }} />,
+    },
+    {
+      path: '/form',
+      element: <FormPage {...{ pageTitle: 'Form', setCurrentPageTitle }} />,
+    },
+    {
+      path: '/404',
+      element: <NotFoundPage {...{ pageTitle: 'Not found(404)', setCurrentPageTitle }} />,
+    },
+    {
+      path: '*',
+      element: <Navigate to="/404" replace />,
+    },
+  ];
+  return (
+    <Routes>
+      {routes.map((route) => {
+        const { path, element } = route;
+        return <Route path={path} element={element} key={path} />;
+      })}
+    </Routes>
+  );
+};
 
 export default Router;
