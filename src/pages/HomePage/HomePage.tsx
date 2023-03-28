cda4afa7e2764dc498c8a7d3fa8bfb514e9b136d
Dudarik <Dudarik@yandex.ru>
2023-03-28 17:05:13 +0500

    refactor: HomePage from class to hooks

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,24 +4,44 @@ import CardsList from '../../components/CardsList';
 import SearchBar from '../../components/SearchBar';
 import React from 'react';
 
-import Page from '../Page';
 import { getGamesData } from '../../helpers';
 
 import styles from './HomePage.module.scss';
 
-class HomePage extends Page {
-  render() {
-    const cardsData = getGamesData() as IGamesData;
-    return (
-      <main className={styles.main}>
-        <h2>
-          This is a <span className="pageTitle">{this.pageTitle}</span> page
-        </h2>
-        <SearchBar />
-        <CardsList {...cardsData} />
-      </main>
-    );
-  }
-}
+import { IPageProps } from 'interfaces/pagesInterfaces';
+
+const HomePage = (props: IPageProps) => {
+  const { pageTitle, setCurrentPageTitle } = props;
+  const cardsData = getGamesData() as IGamesData;
+
+  if (setCurrentPageTitle) setCurrentPageTitle(String(pageTitle));
+
+  return (
+    <main className={styles.main}>
+      <h2>
+        This is a <span className="pageTitle">{pageTitle}</span> page
+      </h2>
+      <SearchBar />
+      <CardsList {...cardsData} />
+    </main>
+  );
+};
 
 export default HomePage;
+
+// class HomePage extends Page {
+//   render() {
+//     const cardsData = getGamesData() as IGamesData;
+//     return (
+//       <main className={styles.main}>
+//         <h2>
+//           This is a <span className="pageTitle">{this.pageTitle}</span> page
+//         </h2>
+//         <SearchBar />
+//         <CardsList {...cardsData} />
+//       </main>
+//     );
+//   }
+// }
+
+// export default HomePage;
