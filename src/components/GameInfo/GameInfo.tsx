7ad1a37c4e384d906f91690d170cf0a462c6da60
Dudarik <Dudarik@yandex.ru>
2023-03-28 16:01:48 +0500

    refactor: GameInfo from class to props

--- src/components/GameInfo/GameInfo.tsx
+++ src/components/GameInfo/GameInfo.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from 'react';
+import React from 'react';
 
 import styles from './GameInfo.module.scss';
 
@@ -10,50 +10,48 @@ interface IGameInfoProps {
   lang: string;
 }
 
-class GameInfo extends Component<IGameInfoProps> {
-  render(): ReactNode {
-    const { releaseDate, players, age, playingTime, lang } = this.props;
-    const infoData = [
-      {
-        icon: 'fa-solid fa-person',
-        title: 'Кол-во игроков:',
-        title_info: players,
-      },
-      {
-        icon: 'fa-solid fa-clock',
-        title: 'Время партии:',
-        title_info: `${playingTime} мин.`,
-      },
-      {
-        icon: 'fa-solid fa-child-reaching',
-        title: 'Возраст:',
-        title_info: `от ${age} лет`,
-      },
-      {
-        icon: 'fa-solid fa-language',
-        title: 'Язык:',
-        title_info: lang,
-      },
-      {
-        icon: 'fa-solid fa-calendar-days',
-        title: 'Дата релиза:',
-        title_info: releaseDate,
-      },
-    ];
-    return (
-      <div className={styles.info}>
-        {infoData.map((infoItem) => {
-          const { icon, title, title_info } = infoItem;
-          return (
-            <span className={styles.info_item} key={icon}>
-              <i className={icon}></i> {title}
-              <span className={styles.item_data}> {title_info}</span>
-            </span>
-          );
-        })}
-      </div>
-    );
-  }
-}
+const GameInfo = (props: IGameInfoProps) => {
+  const { releaseDate, players, age, playingTime, lang } = props;
+  const infoData = [
+    {
+      icon: 'fa-solid fa-person',
+      title: 'Кол-во игроков:',
+      title_info: players,
+    },
+    {
+      icon: 'fa-solid fa-clock',
+      title: 'Время партии:',
+      title_info: `${playingTime} мин.`,
+    },
+    {
+      icon: 'fa-solid fa-child-reaching',
+      title: 'Возраст:',
+      title_info: `от ${age} лет`,
+    },
+    {
+      icon: 'fa-solid fa-language',
+      title: 'Язык:',
+      title_info: lang,
+    },
+    {
+      icon: 'fa-solid fa-calendar-days',
+      title: 'Дата релиза:',
+      title_info: releaseDate,
+    },
+  ];
+  return (
+    <div className={styles.info}>
+      {infoData.map((infoItem) => {
+        const { icon, title, title_info } = infoItem;
+        return (
+          <span className={styles.info_item} key={icon}>
+            <i className={icon}></i> {title}
+            <span className={styles.item_data}> {title_info}</span>
+          </span>
+        );
+      })}
+    </div>
+  );
+};
 
 export default GameInfo;
