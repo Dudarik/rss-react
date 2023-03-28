c8d00458ff8ebc36f688c9743bddf9132cdc5d10
Dudarik <Dudarik@yandex.ru>
2023-03-28 15:55:08 +0500

    refactor: MainNav from class to hooks

--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import styles from './MainNav.module.scss';
@@ -7,32 +7,30 @@ interface IMenuLink {
   isActive: boolean;
 }
 
-class MainNav extends Component {
-  render(): ReactNode {
-    const isActiveLink = (link: IMenuLink) => (link.isActive ? styles.active : styles.menu_link);
-    const links = [
-      { path: '/', title: 'Home' },
-      { path: '/form', title: 'Form' },
-      { path: '/about', title: 'About us' },
-    ];
+const MainNav = () => {
+  const isActiveLink = (link: IMenuLink) => (link.isActive ? styles.active : styles.menu_link);
+  const links = [
+    { path: '/', title: 'Home' },
+    { path: '/form', title: 'Form' },
+    { path: '/about', title: 'About us' },
+  ];
 
-    return (
-      <nav>
-        <ul className={styles.menu}>
-          {links.map((menuItem) => {
-            const { title, path } = menuItem;
-            return (
-              <li key={title}>
-                <NavLink to={path} className={isActiveLink}>
-                  {title}
-                </NavLink>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    );
-  }
-}
+  return (
+    <nav>
+      <ul className={styles.menu}>
+        {links.map((menuItem) => {
+          const { title, path } = menuItem;
+          return (
+            <li key={title}>
+              <NavLink to={path} className={isActiveLink}>
+                {title}
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
 
 export default MainNav;
