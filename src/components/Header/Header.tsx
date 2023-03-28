bd158b4d7d830bf3cc4ad0002cf0336445d2156c
Dudarik <Dudarik@yandex.ru>
2023-03-28 15:58:43 +0500

    refactor: Header from class to hooks

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import MainNav from '../MainNav';
@@ -9,18 +9,16 @@ export interface IPageProps {
   pageTitle?: string;
 }
 
-class Header extends Component<IPageProps> {
-  render() {
-    return (
-      <header className={styles.header}>
-        <Link to="/">
-          <h1 className={styles.logo}>RSS-React</h1>
-        </Link>
-        <h2>Current page: {this.props.pageTitle || 'untitled'}</h2>
-        <MainNav />
-      </header>
-    );
-  }
-}
+const Header = (props: IPageProps) => {
+  return (
+    <header className={styles.header}>
+      <Link to="/">
+        <h1 className={styles.logo}>RSS-React</h1>
+      </Link>
+      <h2>Current page: {props.pageTitle || 'untitled'}</h2>
+      <MainNav />
+    </header>
+  );
+};
 
 export default Header;
