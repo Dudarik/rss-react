d2b51c2056a7c22b7b35d208223857c3dad916d9
Dudarik <Dudarik@yandex.ru>
2023-03-28 16:10:47 +0500

    refactor: CustomRadioBox from class to hooks

--- src/components/CustomRadioBox/CustomRadioBox.tsx
+++ src/components/CustomRadioBox/CustomRadioBox.tsx
@@ -1,27 +1,25 @@
 import { IRadioBox } from 'interfaces/formInterfaces';
-import React, { Component, ReactNode } from 'react';
+import React from 'react';
 
 import styles from './CustomRadioBox.module.scss';
 
-class CustomRadioBox extends Component<IRadioBox> {
-  render(): ReactNode {
-    const { dataArr, name, title } = this.props;
-    return (
-      <fieldset className={styles.radioFieldset}>
-        <legend>{title}</legend>
-        {dataArr.map((item) => {
-          const { id, value, refProp } = item;
+const CustomRadioBox = (props: IRadioBox) => {
+  const { dataArr, name, title } = props;
+  return (
+    <fieldset className={styles.radioFieldset}>
+      <legend>{title}</legend>
+      {dataArr.map((item) => {
+        const { id, value, refProp } = item;
 
-          return (
-            <label htmlFor={id} key={id}>
-              <input type="radio" name={name} id={id} value={value} ref={refProp} />
-              {value}
-            </label>
-          );
-        })}
-      </fieldset>
-    );
-  }
-}
+        return (
+          <label htmlFor={id} key={id}>
+            <input type="radio" name={name} id={id} value={value} ref={refProp} />
+            {value}
+          </label>
+        );
+      })}
+    </fieldset>
+  );
+};
 
 export default CustomRadioBox;
