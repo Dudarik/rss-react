67a1742b66645fa9d3fc3483038be6a640884a41
Dudarik <Dudarik@yandex.ru>
2023-03-28 16:03:05 +0500

    refactor: Footer from class to hooks

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import styles from './Footer.module.scss';
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className={styles.footer}>
-        <h4>
-          Copyright 2023 &#169; <a href="https://github.com/Dudarik">Alex Reznichenko</a> - RSSchool
-        </h4>
-      </footer>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h4>
+        Copyright 2023 &#169; <a href="https://github.com/Dudarik">Alex Reznichenko</a> - RSSchool
+      </h4>
+    </footer>
+  );
+};
 
 export default Footer;
